d64b3a1e4043dde45461dc38b20ea0ac7ddd3a7a
61070091 <45088351+61070091@users.noreply.github.com>
2018-12-15 17:49:21 +0700

    Grap Bar & Donut

---
+++ PhotoGraphic/Donut.py
@@ -0,0 +1,32 @@
+"""project"""
+
+import pygal
+
+def calculate():
+	"""calculate Donut"""
+
+
+
+def picture():
+	"""picture Donut"""
+	pie_chart = pygal.Pie()
+	pie_chart.title = 'Director (in %)'
+	pie_chart.add('Action', 19.5)
+	pie_chart.add('Adventure', 36.6)
+	pie_chart.add('War', 36.3)
+	pie_chart.add('Drama', 4.5)
+	pie_chart.add('Science', 2.3)
+	pie_chart.add('Family', 2.3)
+	pie_chart.add('Thriller', 2.3)
+	pie_chart.add('Crime', 2.3)
+	pie_chart.add('Documentaries', 2.3)
+	pie_chart.add('Animation ', 2.3)
+	pie_chart.add('Comedy ', 36.6)
+	pie_chart.add('Erotic', 36.3)
+	pie_chart.add('Fantasy', 4.5)
+	pie_chart.add('Musicals', 2.3)
+	pie_chart.add('Romance ', 2.3)
+	pie_chart.add('Western ', 2.3)
+	pie_chart.render_to_file('pie-chart.svg')
+
+picture()
