2bcab4402384d0d236ab80a0696c7d014dc45713
61070091 <45088351+61070091@users.noreply.github.com>
2018-12-17 18:01:45 +0700

    commit files

---
+++ Graph/Bar_vote/exbar2.py
@@ -0,0 +1,35 @@
+"""Project"""
+
+import pygal
+import pandas as pd
+from ast import literal_eval
+
+def open(dir_name):
+    """openfile,last_data"""
+    movie_type = {}
+    filedata = pd.read_csv('completed_movie_database_for_PSIT.csv')
+    top1 = filedata.loc[filedata['director'] == dir_name, ["title", "vote_average", "revenue"]]
+    for i in range(len(top1.index)):
+        currenttitle = top1.iloc[i]['title']
+        if currenttitle in movie_type:
+            movie_type[currenttitle] += top1.iloc[i]['vote_average']
+        else:
+            movie_type[currenttitle] = top1.iloc[i]['vote_average']
+    print("Data Success!!")
+    return movie_type
+
+
+    # filedata.close()
+
+def picture(subject):
+    """Picture Pie"""
+    newdict = open(subject)
+    bar_chart = pygal.Bar()
+    bar_chart.title = subject+"'s movies genres."
+    lis = sorted(newdict.items(), key=lambda x: x[1], reverse=True)
+    for i in lis:
+        bar_chart.add(i[0], i[1])
+    bar_chart.render_to_file('%s_bar.svg' % subject)
+    print("Done!!")
+
+picture(input())
