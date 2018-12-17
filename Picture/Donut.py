72be3c8e827535d79557e123e4a2512dda56b281
61070091 <45088351+61070091@users.noreply.github.com>
2018-12-17 15:12:35 +0700

    Update Donut.py

--- Picture/Donut.py
+++ Picture/Donut.py
@@ -1,11 +1,29 @@
 """project"""
 
 import pygal
+import pandas as pd
+from ast import literal_eval
+
+def open():
+	"""openfile,last_data"""
+	movie_type = {}
+	filedata = pd.DataFrame(r '\Users\Mickey\Documents\completed_movie_database_for_PSIT.csv')
+	top1 = filedata.loc[filedata['director'] == 'James Cameron', ["genres"]]
+	for i in range(len(top1.index)):
+		for j in literal_eval(top1.iloc[i]['genres']):
+			if j['name'] in movie_type:
+				movie_type['name'] += 1
+			else:
+				movie_type['name'] = 1
+	return(movie_type)
+
+
+	# filedata.close()
 
 def calculate():
 	"""calculate Donut"""
-
-
+	name = input()
+	
 
 def picture():
 	"""picture Donut"""
@@ -29,4 +47,5 @@ def picture():
 	pie_chart.add('Western ', 2.3)
 	pie_chart.render_to_file('pie-chart.svg')
 
-picture()
+# picture()
+print(open())
